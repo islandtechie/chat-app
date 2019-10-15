1e7aa5cbf5c734260bc1e802dcf40a085040ecbd
Berlin Smith <berlin.smithjr@gmail.com>
2019-10-15 18:51:32 -0400

    moved html into react app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <p>hello</p>
+        <h1>Chatty!</h1>
+        <div className="chat-container">
+          <div className="users">
+            <h3>Users</h3>
+            <p>Berlin S. (You)</p>
+          </div>
+          <div className="chat-section">
+                <div className="chat-window">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+                <div className="chat-controls">
+                    <form action="#">
+                        <input type="text" />
+                        <button type="submit">SEND</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
