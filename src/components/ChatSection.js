46b45c51a20c6474257c763f29e191692480676d
Berlin Smith <berlin.smithjr@gmail.com>
2019-10-19 15:05:54 -0400

    removed some dummy data

--- src/components/ChatSection.js
+++ src/components/ChatSection.js
@@ -12,7 +12,7 @@ class ChatSection extends Component {
             {
               "_id": "5dab4d53169e50eb1e545cb4",
               "name": "Ebony Vinson",
-              "about": "Officia velit mollit laborum amet nisi fugiat id est. Est laboris do quis velit id et exercitation ipsum. Velit deserunt exercitation Lorem officia.\r\n"
+              "text": "Officia velit mollit laborum amet nisi fugiat id est. Est laboris do quis velit id et exercitation ipsum. Velit deserunt exercitation Lorem officia.\r\n"
             },
             {
               "id": "5dab4d53b957e4209ff08b02",
@@ -22,22 +22,12 @@ class ChatSection extends Component {
             {
               "id": "5dab4d534d211b945831f855",
               "name": "Sampson Shelton",
-              "text": "Velit ad tempor cupidatat commodo Lorem tempor sit ad. Ipsum in quis dolor sit anim nostrud. Consequat quis labore occaecat enim nisi. Ipsum deserunt officia ut nisi ipsum ullamco tempor fugiat do. Laboris cillum eu aute est. Deserunt Lorem incididunt ex eiusmod deserunt consectetur.\r\n"
-            },
-            {
-              "id": "5dab4d53d77ed8cab20b9adf",
-              "name": "Roxanne Lawson",
-              "text": "Consectetur ex enim incididunt et qui. Ex dolor qui sit do laborum ad nostrud proident nisi. Occaecat irure sint pariatur ut minim. Adipisicing officia in nisi exercitation duis nisi. Voluptate duis ad dolore velit.\r\n"
-            },
-            {
-              "id": "5dab4d533ba0477b23aa1059",
-              "name": "Tate Gamble",
-              "text": "Ex fugiat reprehenderit aute et nisi labore reprehenderit eiusmod nisi. Amet occaecat anim voluptate elit magna. Ut aute exercitation qui in nulla irure fugiat. Eu qui occaecat ullamco laboris tempor duis esse ex minim commodo anim laboris.\r\n"
+              "text": "Velit ad tempor cupidatat commodo Lorem tempor sit ad. Ipsum in quis dolor sit anim nostrud. \r\n"
             },
             {
               "id": "5dab4d5303d7d5b46564daea",
               "name": "Moses Kane",
-              "text": "Sunt deserunt proident velit ea occaecat dolor culpa excepteur sint sunt est occaecat laboris. Do esse nisi cillum aute dolor nostrud dolor sunt esse cillum. Lorem ipsum fugiat nulla esse occaecat proident ipsum dolor. Ullamco esse ipsum et Lorem occaecat laborum exercitation ex in in ullamco ea est. Veniam labore ipsum laborum quis officia nisi irure.\r\n"
+              "text": "Sunt deserunt proident velit ea occaecat dolor culpa excepteur sint sunt est occaecat laboris. \r\n"
             }
           ]
     }
