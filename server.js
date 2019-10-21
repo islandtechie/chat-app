d05a9ed5365756d2daca90e4e6d10369c42ae8b6
Berlin Smith <berlin.smithjr@gmail.com>
2019-10-20 22:55:48 -0400

    Server working

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000;
 
-app.get('/', (req,res) => {
-    res.send('it wworks part 1');
-})
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/hey', (req, res) => {
+    res.send('yooooo');
+});
 
 app.listen(PORT, () => { return `Listening on port ${PORT}...`});
