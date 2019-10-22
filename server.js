344573cae7992d4977e189ef05d25920d23b368d
Berlin Smith <berlin.smithjr@gmail.com>
2019-10-21 21:15:44 -0400

    Routes created
    Route structure created moving to putting dummy data to test api calls

--- server.js
+++ server.js
@@ -9,8 +9,27 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/hey', (req, res) => {
-    res.send('yooooo');
+app.get('/api/messages', (req, res) => {
+    res.send('messages');
+    console.log('from messages');
 });
 
+app.post('/api/messages', (req, res) => {
+    res.send('post messages');
+    console.log('from post messages');
+});
+
+app.get('/api/users', (req, res) => {
+    res.send('get users');
+    console.log('from users');
+});
+
+app.post('/api/users', (req, res) => {
+    res.send('post uers');
+    console.log('from post users');
+});
+
+app.get('/api/users/{:id}', (req, res) => {
+    res.send(req.params.id);
+});
 app.listen(PORT, () => { return `Listening on port ${PORT}...`});
